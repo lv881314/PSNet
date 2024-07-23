199db63749db44be9cdf54f31cf81ab049acecd5
lv881314 <63967629+lv881314@users.noreply.github.com>
2024-07-23 12:21:42 +0800

    Update PSNet.py

--- PSNet.py
+++ PSNet.py
@@ -11,8 +11,6 @@ from Models1.utils import conv_bn_relu
 
 
 
-
-
 class PSNet(nn.Module):
     def __init__(self, backbone,  sync_bn=True, pretrained=True, ResNet34M= False, criterion=nn.CrossEntropyLoss(ignore_index=255), classes = 6):
         super(SSFPN, self).__init__()
@@ -48,18 +46,23 @@ class PSNet(nn.Module):
         self.conv4_x = encoder.layer3  # 1/16
         self.conv5_x = encoder.layer4  # 1/32
 
-        self.decoder = DecoderBlock(self.out_channels[4], self.out_channels[4], BatchNorm) #
+        self.decoder = MDPM(self.out_channels[4], self.out_channels[4], BatchNorm) #
 
         self.down2 = conv_block(self.out_channels[-4], self.out_channels[1], 3, 1, 1, 1, 1, bn_act=True)
         self.down3 = conv_block(self.out_channels[-3], self.out_channels[2], 3, 1, 1, 1, 1, bn_act=True)
         self.down4 = conv_block(self.out_channels[-2], self.out_channels[3], 3, 1, 1, 1, 1, bn_act=True)
         self.down5 = conv_block(self.out_channels[-1], self.out_channels[4], 3, 1, 1, 1, 1, bn_act=True)
 
+        self.apf1 = FWM(self.out_channels[4], self.out_channels[4], self.out_channels[3], classes=classes)
+        self.apf2 = FWM(self.out_channels[3], self.out_channels[3], self.out_channels[2], classes=classes)
+        self.apf3 = FWM(self.out_channels[2], self.out_channels[2], self.out_channels[1], classes=classes)
+        self.apf4 = FWM(self.out_channels[1], self.out_channels[1], self.out_channels[0], classes=classes)
 
-        self.classifier = SegHead(self.out_channels[1], classes)
+
+        self.classifier = SegHead(self.out_channels[0], classes)
 
         self.D = LCGB(3, self.out_channels[1])
-        self.saff = SAFF()
+        self.saff = SWAM()
 
     def forward(self, x, y=None, z= None):
         B, C, H, W = x.size()
@@ -86,12 +89,56 @@ class PSNet(nn.Module):
             x5 = self.down5(x5)
             
         cfgb1 = self.decoder(x5) #torch.Size([6, 512, 16, 16])
+        D112 = torch.add(DH, x2) #torch.Size([6, 64, 160, 160])
+
+        APF1 = self.apf1(cfgb1, x5)
+        APF2 = self.apf2(APF1, x4)
+        APF3 = self.apf3(APF2, x3)
+        APF4 = self.apf4(APF3, D112) #x2
         
+        APF = self.saff(APF4)
 
-
-
+        classifier1 = self.classifier(APF41)
         predict = F.interpolate(classifier1, size=(H, W), mode="bilinear", align_corners=True)
+        
+        if self.training:
+            main_loss = self.criterion(predict, y)
+            # axu_loss = 1/4 * self.criterion(s1, y) + 1/8 * self.criterion(s2, y) + 1/16 * self.criterion(s3, y) + 1/32 * self.criterion(s4, y)
+            # main_loss = main_loss + axu_loss
+            return predict.max(1)[1], main_loss, main_loss
+        else:
+            return predict
+
         return predict
+
+class SWAM(nn.Module):
+    def __init__(self, a=0.5, b=2, sigma=0.0001):
+        super(SWAM, self).__init__()
+        self.a = a
+        self.b = b
+        self.sigma = sigma
+
+    def forward(self, x):
+        """
+        :param x: (n, c, h, w)
+        :return:
+        """
+        n, K, h, w = x.shape
+        S = x.sum(dim=1)  # n,h,w
+        z = torch.sum(S ** self.a, dim=[1, 2])
+        z = (z ** (1 / self.a)).view(n, 1, 1)
+        S = (S / z) ** (1 / self.b)
+        S = S.unsqueeze(1)
+        AW = (x * S).sum(dim=[2, 3])
+        # print(new_x.size())
+        AW = AW.unsqueeze(2).unsqueeze(3)
+        AW = AW.expand_as(x)
+        AW1 = AW * x
+        new = x + AW1
+        return new
+
+
+
 class LCGB(nn.Module):
     def __init__(self, in_ch, out_ch):
         super().__init__()
@@ -161,9 +208,9 @@ class NeighborDecouple(nn.Module):
         x = rearrange(x, 'b c (h h2) (w w2) -> b (h2 w2 c) h w', h2=self.bs, w2=self.bs)
         return x
 # 全局
-class DecoderBlock(nn.Module):
+class MDPM(nn.Module):
     def __init__(self, in_channels, n_filters, BatchNorm, inp=False):
-        super(DecoderBlock, self).__init__()
+        super(MDPM, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, in_channels // 4, 1)
         self.bn1 = BatchNorm(in_channels // 4)
         self.relu1 = nn.ReLU()
@@ -272,3 +319,61 @@ class DecoderBlock(nn.Module):
         x = x.reshape(shape[0], shape[1], shape[-2], 2*shape[-2])
         x = x[..., 0: shape[-2]]
         return x.permute(0, 1, 3, 2)
+        
+class FWM(nn.Module):
+    def __init__(self, channels_high, channels_low, channel_out, classes=11):
+        super(FWM, self).__init__()
+
+        self.lateral_low = conv_block(channels_low, channels_high, 1, 1, bn_act=True, padding=0)
+
+        self.conv_low = conv_block(channels_high, channel_out, 3, 1, bn_act=True, padding=1)
+        self.sa = SpatialAttention(channel_out, channel_out)
+
+        self.conv_high = conv_block(channels_high, channel_out, 3, 1, bn_act=True, padding=1)
+        self.ca = ChannelWise(channel_out)
+
+        self.FRB = nn.Sequential(
+            conv_block(2 * channels_high, channel_out, 1, 1, bn_act=True, padding=0),
+            conv_block(channel_out, channel_out, 3, 1, bn_act=True, group=1, padding=1))
+
+        self.classifier = nn.Sequential(
+            conv_block(channel_out, channel_out, 3, 1, padding=1, group=1, bn_act=True),
+            nn.Dropout(p=0.15),
+            conv_block(channel_out, classes, 1, 1, padding=0, bn_act=False))
+        self.apf = conv_block(channel_out, channel_out, 3, 1, padding=1, group=1, bn_act=True)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.active = nn.Sigmoid()
+
+    def forward(self, x_high, x_low):
+        _, _, h, w = x_low.size()
+
+        lat_low = self.lateral_low(x_low)
+
+        high_up1 = F.interpolate(x_high, size=lat_low.size()[2:], mode='bilinear', align_corners=False)
+
+        concate = torch.cat([lat_low, high_up1], 1)
+        concate = self.FRB(concate)
+
+        conv_high = self.conv_high(high_up1)
+        conv_low = self.conv_low(lat_low)
+
+        sa = self.sa(concate)
+        ca = self.ca(concate)
+
+        mul1 = torch.mul(sa, conv_high)
+        mul2 = torch.mul(ca, conv_low)
+
+        # mul1 = mul1 + conv_high
+        # mul2 = mul2 + conv_low
+        att_out = mul1 + mul2
+
+        cat_f = torch.cat((conv_high, conv_low, att_out), dim=1)
+        b, mc, _, _ = cat_f.size()
+        act_f = self.active(torch.abs(cat_f))
+        avg_f = self.avg_pool(act_f)
+        weight = torch.split(avg_f, mc // 3, dim=1)
+        out = weight[0] * conv_high + weight[1] * conv_low + weight[2] * (att_out)
+        att_out =out
+
+        APF = self.apf(att_out)
+        return APF
